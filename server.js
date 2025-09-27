a5b31a911d35910986dab38a2ff8323b834e96bc
Quentin Jeriko <quentin.jeriko@gmail.com>
2025-09-27 03:47:12 -0700

    added method within the object and demonstrated understanding of how to use the method with an instance of the object

--- server.js
+++ server.js
@@ -1,23 +1,37 @@
 function Complete_Name(first_name, last_name) {
   this.first_name = first_name;
   this.last_name = last_name;
+
+
+  this.returnCompleteName = () => {
+    return (`${this.first_name} ${this.last_name}`);
+  };
+
+
+
+  this.say = (interjection) => {
+    console.log(`${interjection} ${this.first_name} ${this.last_name}!`)
+  }
+
+
+
 }
 
-const testName = new Complete_Name("Bob", "The Builder");
 
 
-const message = "What's Poppin'";
+const footBallPlayer1 = new Complete_Name("Cooper", "Dejean");
 
-const noun = "World"
+const footBallPlayer2 = new Complete_Name("AJ", "Brown");
+
+const sentence = `Together, the defensive back, ${footBallPlayer1.returnCompleteName()}, and the wide receiver, ${footBallPlayer2.returnCompleteName()}, make a great team!`
 
 
 
+footBallPlayer1.say("You got some huge shoes to fill,")
+
+footBallPlayer2.say("You got Aura,")
+
+console.log(sentence)
 
 
-sayMsg = (msg) => {
-    console.log(msg);
-}
 
-sayMsg(`${message} ${testName.first_name} ${testName.last_name}!`);
-
-console.log(`Hello ${noun}`)
