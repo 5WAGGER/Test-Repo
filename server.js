ddf6a938e58372874998a7529f714ca1685af559
Quentin Jeriko <quentin.jeriko@gmail.com>
2025-09-27 02:39:10 -0700

    added a constructor function and then initializes the instance of the object Complete_Name

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
+function Complete_Name(first_name, last_name) {
+  this.first_name = first_name;
+  this.last_name = last_name;
+}
 
+const testName = new Complete_Name("Bob", "The Builder");
 
 
 const message = "What's Poppin'";
 
-const message2 = "World"
+const noun = "World"
+
+
+
+
 
 sayMsg = (msg) => {
     console.log(msg);
 }
 
-sayMsg(`${message} ${message2}!`);
+sayMsg(`${message} ${testName.first_name} ${testName.last_name}!`);
+
+console.log(`Hello ${noun}`)
