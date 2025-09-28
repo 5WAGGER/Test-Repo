4c46979546595f057206d5d98d773e2735f683f2
Quentin Jeriko <quentin.jeriko@gmail.com>
2025-09-28 03:55:55 -0700

    added new property to completeName constructor

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-function Complete_Name(first_name, last_name) {
+function Complete_Name(first_name, last_name, team) {
   this.first_name = first_name;
   this.last_name = last_name;
+  this.team = team;
 
 
   this.returnCompleteName = () => {
@@ -15,15 +16,16 @@ function Complete_Name(first_name, last_name) {
 
 
 
+
 }
 
 
 
-const footBallPlayer1 = new Complete_Name("Cooper", "Dejean");
+const footBallPlayer1 = new Complete_Name("Cooper", "Dejean", "Philadelphia Eagles");
 
-const footBallPlayer2 = new Complete_Name("AJ", "Brown");
+const footBallPlayer2 = new Complete_Name("AJ", "Brown", "Philadelphia Eagles");
 
-const sentence = `Together, the defensive back, ${footBallPlayer1.returnCompleteName()}, and the wide receiver, ${footBallPlayer2.returnCompleteName()}, make a great team!`
+const sentence = `Together, the defensive back, ${footBallPlayer1.returnCompleteName()}, and the wide receiver, ${footBallPlayer2.returnCompleteName()}, make the great ${footBallPlayer2.team}!`
 
 
 
